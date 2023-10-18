50105b5e25f5da28e183c8973714052068093f93
chiragpatel222 <chirag.gscb@gmail.com>
2023-10-18 16:34:39 +1300

    ListGroup with selection

---
+++ FirstProject/src/components/ListGroup.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function ListGroup() {
+  let items = ["First", "Second", "Third", "Fourth"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>no item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
